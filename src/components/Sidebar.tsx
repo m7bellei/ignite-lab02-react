ac6bfa11de0e67b8edc1418fd42c14cefa018fff
m7bellei <mateus_xdzin@hotmail.com>
2022-06-25 13:30:14 -0300

    sidebar receving the lesson cards dynamic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,48 @@
+import { gql, useQuery } from "@apollo/client";
 import { LessonCards } from "./LessonCards";
 
+const GET_LESSONS_QUERY = gql`
+    query GET_LESSONS_QUERY {
+        lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+            id
+            lessonType
+            availableAt
+            title
+            slug
+        }
+    }
+`;
+
+interface GetLessonsQueryResponse {
+    lessons: {
+        id: string;
+        lessonType: 'live' | 'class';
+        availableAt: string;
+        title: string;
+        slug: string;
+    }[];
+}
+
 export function Sidebar() {
+    const {data} = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
                 Cronograma de aulas
             </span>
             <div className="flex flex-col gap-8">
-                <LessonCards/>
-                <LessonCards/>
-                <LessonCards/>
-                <LessonCards/>
-                <LessonCards/>
-                <LessonCards/>
+                {data?.lessons.map(lesson => {
+                    return (
+                        <LessonCards
+                            key={lesson.id}
+                            title={lesson.title}
+                            slug={lesson.slug}
+                            availableAt={new Date(lesson.availableAt)}
+                            type={lesson.lessonType}
+                        />
+                    )
+                })}
             </div>
         </aside>
     )
