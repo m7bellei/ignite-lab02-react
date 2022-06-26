2310b2a896ec4acd492ee88ac44e93847037a30c
m7bellei <mateus_xdzin@hotmail.com>
2022-06-26 18:03:58 -0300

    Finished static page subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,44 @@
+import { Logo } from "../components/Logo";
+
 export function Subscribe() {
     return (
-       <h1>subscribe</h1>
-    )
+       <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+                <div className="max-w-[640px]">
+                    <Logo/>
+                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, 
+                        com <strong className="text-blue-500">React JS</strong>
+                    </h1>
+                    <p className="mt-4 text-gray-200 leading-relaxed">
+                        Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda 
+                        para acessar as melhores oportunidades do mercado.
+                    </p>
+                </div>
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                    <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+
+                    <form action="" className="flex flex-col gap-2 w-full px-5">
+                        <input 
+                            type="text" 
+                            className="w-full px-5 h-14 bg-gray-900 rounded" 
+                            placeholder="Digite seu nome completo"
+                        />
+                        <input 
+                            type="email" 
+                            className="w-full px-5 h-14 bg-gray-900 rounded" 
+                            placeholder="Digite seu melhor email"
+                        />
+                        <button 
+                            type="submit"
+                            className="mt-4 bg-green-500 uppercase py-4 font-bold text-sm hover:bg-green-700 transition-colors rounded"
+                        >
+                            Inscrever-se
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <img src="/src/assets/code-mockup.png" className="mt-10" alt="Code Mockup" />
+       </div>
+    );
 }
